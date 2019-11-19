ee975ec6ec33ddc7d5d27d8920a9afcb0cef2387
hyun1014 <ksp70712@gmail.com>
2019-11-20 00:52:31 +0900

    First attempt(Inputsize=8, shape=10, batchsize=6)

--- model/testing/naver_test1.py
+++ model/testing/naver_test1.py
@@ -98,12 +98,12 @@ print(train_x)
 print(x_train)
 
 model = models.Sequential()
-model.add(layers.Dense(5, activation='relu', input_shape=(10,)))
-model.add(layers.Dense(5, activation='relu'))
+model.add(layers.Dense(8, activation='relu', input_shape=(10,)))
+model.add(layers.Dense(8, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
 model.compile(optimizer=optimizers.RMSprop(lr=0.001),
-              loss=losses.binary_crossentropy(),
+              loss=losses.binary_crossentropy,
               metrics=[metrics.binary_accuracy])
 
 model.fit(x_train, y_train, epochs=10, batch_size=6)
