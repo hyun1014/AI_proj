878192f03f52fd38e311571b9574694ef278fa80
hyun1014 <ksp70712@gmail.com>
2019-11-19 18:18:03 +0900

    Requirement is changed

--- model/testing/pharse_analyze.py
+++ model/testing/pharse_analyze.py
@@ -1,3 +1,25 @@
 import nltk
 from konlpy.tag import Okt
 
+twit = Okt()
+sentence = u"만 19세 미만 청소년은 관람이 불가합니다."
+words = twit.pos(sentence)
+
+gram = """
+NP: {<N.*>*<Suffix>?}   # Noun phrase
+VP: {<V.*>*}            # Verb phrase
+AP: {<A.*>*}            # Adjective phrase
+"""
+
+print(gram)
+parser = nltk.RegexpParser(gram)
+chunks = parser.parse(words)
+print("Whole trees")
+print(chunks.pprint())
+
+print("Only nouns")
+for subtree in chunks.subtrees():
+    if subtree.label()=="NP":
+        print(' '.join(e[0] for e in list(subtree)))
+        print(subtree.pprint())
+
