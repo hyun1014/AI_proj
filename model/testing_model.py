ba14839ea333f635a2c48820dea3cc97c1086ddc
hyun1014 <ksp70712@gmail.com>
2019-11-21 19:58:59 +0900

    testing_model.py is created

---
+++ model/testing_model.py
@@ -0,0 +1,37 @@
+import json
+import sys
+import nltk
+import numpy as np
+from keras import utils, datasets, models, layers
+from keras.models import load_model, save_model
+
+target_model = "NLP_model/" + sys.argv[1] + ".h5"
+target_json = "json_files/" + sys.argv[2] + ".json"
+# Load json file for test
+with open(target_json) as f:
+    test_target = json.load(f)
+
+# Load selected_word
+with open("NLP_model/selected_word.json", "r") as sf:
+    selected_word = json.load(sf)
+# Load model
+model = load_model(target_model)
+
+
+def term_frequency(doc):
+    return [doc.count(s_word) for s_word in selected_word]
+
+
+# Vectorization
+# x is input (term), and it is dataset
+test_x = [term_frequency(d) for d, _ in test_target]
+# y is output (0 or 1), and it is dataset
+test_y = [c for _, c in test_target]
+
+# Change x and y to float type data
+test_x = np.asarray(test_x).astype("float32")
+test_y = np.asarray(test_y).astype("float32")
+
+result = model.evaluate(test_x, test_y)
+test_accuracy = result[1] * 100
+print("Accuracy is {:.2f}%".format(test_accuracy))
